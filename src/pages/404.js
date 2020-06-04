6a5a2bca184da43187d79a9b149e0e40740baa38
Alona Trekhlib <trekhleb.al@gmail.com>
2020-06-04 17:30:20 -0500

    custom page not found

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,27 @@
-import React from "react"
+import React, { useEffect } from "react"
+import Footer from "./footer"
+import "./style.css"
 
 export default function Error() {
-  return <div>Page not found</div>
+  useEffect(() => {
+    document.title = "alona trekhlib - page not found"
+  })
+  return (
+    <>
+      <div>
+        <div className="main">
+          <Link to="/">
+            <FaHome id="home-icon" />
+          </Link>
+          <div className="about-wrapper">
+            <div className="about">
+              Not Found
+              <p>Ooops, page not found :(</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
 }
